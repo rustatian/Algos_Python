ffbb21fc50ae853f9b611b0d74cebb745d5276a8
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-02-15 12:54:23 +0100

    chore: add test
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- LeetCode/bw/176/A.py
+++ LeetCode/bw/176/A.py
@@ -6,9 +6,45 @@ class Solution:
             wl = len(w)
             s = 0
             for ch in w:
-                s += weights[ord(ch) - ord('a')]
+                s += weights[ord(ch) - ord("a")]
             s = s % 26
-            res += chr(ord('z') - s)
+            res += chr(ord("z") - s)
             st += wl
 
         return res
+
+
+print(
+    Solution().mapWordWeights(
+        ["abc", "bcd"],
+        [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+        ],
+    )
+)
+
