c8ff58209f283a2534146f972bc87b379dbea301
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-04-19 19:48:20 +0200

    chore: find missing ranges
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ LeetCode/easy/missing_ranges.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/missing-ranges
+
+
+class Solution:
+    def findMissingRanges(
+        self, nums: list[int], lower: int, upper: int
+    ) -> list[list[int]]:
+        if len(nums) == 0:
+            return [[lower, upper]]
+
+        res: list[list[int]] = []
+        if lower < nums[0]:
+            res.append([lower, nums[0] - 1])
+
+        for i in range(0, len(nums) - 1):
+            if nums[i + 1] - nums[i] == 1:
+                continue
+            res.append([nums[i] + 1, nums[i + 1] - 1])
+
+        if nums[len(nums) - 1] < upper:
+            res.append([nums[len(nums) - 1] + 1, upper])
+
+        return res
+
+
+s = Solution()
+assert s.findMissingRanges([0, 1, 3, 50, 75], 0, 99) == [
+    [2, 2],
+    [4, 49],
+    [51, 74],
+    [76, 99],
+]
